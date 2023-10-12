9d0b67c0111dba43eafa0c1dde7007c75776da86
Katy248 <petrovanton247@gmail.com>
2023-10-12 21:21:02 +0300

    Update profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,22 @@
-Y:/Windows/Documents/PowerShell/CustomShell.ps1 -ThemeRepository "https://github.com/Katy248/kvtheme" -Clear $True -WaitSeconds 0 -EnableLogs $True
+<# Current path from which user runs script #>
+$callPath = Get-Location
+<# Go to profile directory #>
+Set-Location -Path $PROFILE.Replace("Microsoft.PowerShell_profile.ps1", "")
 
-Set-Alias lvim 'C:\Users\Katy\.local\bin\lvim.ps1'
+
+$themeParams = @{
+    ThemeRepository = "https://github.com/Katy248/kvtheme" 
+    Clear           = $True 
+    WaitSeconds     = 0
+    EnableLogs      = $False 
+}
+
+./CustomShell.ps1 @themeParams
+
+<# Windows only #>
+if ($IsWindows) {
+    Set-Alias lvim 'C:\Users\Katy\.local\bin\lvim.ps1'
+}
+
+<# Return to start path #>
+Set-Location -Path $callPath
