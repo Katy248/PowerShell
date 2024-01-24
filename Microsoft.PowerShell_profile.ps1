beef839b293410208d492398a692c31248780a5a
Katy248 <petrovanton247@gmail.com>
2024-01-24 18:54:56 +0300

    Update Set-Theme cmdlet

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,21 +1,21 @@
+$InformationPreference = 'Continue'
 <# Current path from which user runs script #>
 $callPath = Get-Location
 <# Go to profile directory #>
 Set-Location -Path $PROFILE.Replace("Microsoft.PowerShell_profile.ps1", "")
 
-
-$themeParams = @{
-    Clear           = $False 
-    WaitSeconds     = 0
-    EnableLogs      = $True 
-}
-
-./Set-Theme.ps1 @themeParams
+./Set-Theme.ps1 -ThemeFilePath "~/.themes/current/pwsh/theme.omp.json"
 
 <# Windows only #>
 if ($IsWindows) {
     Set-Alias lvim 'C:\Users\Katy\.local\bin\lvim.ps1'
 }
+else {
+    Set-Alias thm '~/ThemeManager/ThemeManager.Cli/bin/Release/net8.0/thm'
+}
 
 <# Return to start path #>
 Set-Location -Path $callPath
+
+Start-Sleep -Seconds 0.8
+Clear-Host
--- Set-Theme.ps1
+++ Set-Theme.ps1
@@ -1,86 +1,17 @@
 param(
-    [string]$ThemeName,
-    [string]$ThemeFilePath,
-    [string]$ThemeRepositoryPath,
-    [bool]$Clear = $True,
-    [double]$WaitSeconds = 0.8,
-    [string]$PrimaryColor = "Magenta",
-    [string]$InfoColor = "Cyan",
-    [string]$SecondaryColor = "DarkGray",
-    [bool]$EnableLogs = $False
+    [string]$ThemeFilePath
 )
 
-function Log {
-    param (
-        [string]$Message
-    )
-    if ([bool]$EnableLogs) {
-        Write-Host $Message -ForegroundColor $InfoColor
-    }
-}
-
-$currentWorkingDirectory = Get-Location
-
-$themeDirectory = "$HOME/.themes/current/pwsh"
-
-if ($ThemeFilePath) {
-    Log "Initialize theme from file '$ThemeFilePath'"
+function InitializeTheme {
     oh-my-posh init pwsh --config "$ThemeFilePath" | Invoke-Expression
 }
-elseif ($ThemeName) {
-    Log "Initialize default theme '$ThemeName'"
-    oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/$ThemeName.omp.json" | Invoke-Expression
+
+Write-Information "Start initializing theme with config: '$ThemeFilePath'"
+
+if (Test-Path -Path $ThemeFilePath) {
+    InitializeTheme
+    Write-Information "Done"
 }
-elseif ($ThemeRepositoryPath) {
-    if (Test-Path -Path "$themeDirectory/.git") {
-        
-        Set-Location $themeDirectory
-        
-        $remote = (git remote get-url origin | Out-String).Trim()
-
-        Write-Host "Current remote '$remote'" -ForegroundColor $PrimaryColor
-
-        if ($remote -eq $ThemeRepositoryPath) {
-            Log -Message "Pulling remote repository to '$themeDirectory'"
-            git pull
-        }
-        else {
-            Log "Delete current remote '$remote'"   
-            Remove-Item -Force -Recurse -Path $themeDirectory
-
-            Log "Cloning new remote '$ThemeRepositoryPath'"
-            git clone $ThemeRepositoryPath $themeDirectory
-        }
-    }
-    else {
-        Log -Message "Cloning remote '$ThemeRepositoryPath'"
-        git clone $ThemeRepositoryPath $themeDirectory
-    }
+else {
+    Write-Warning -Message "File '$ThemeFilePath' doesn't exists"
 }
-else {  
-    Log "Initializing theme '$themeDirectory/theme.omp.json'"
-    oh-my-posh init pwsh --config "$themeDirectory/theme.omp.json" | Invoke-Expression
-} 
-
-Write-Host "Done" -ForegroundColor $PrimaryColor
-
-if ([bool]$Clear) {
-    Write-Host "Wait $WaitSeconds s" -ForegroundColor $SecondaryColor
-    Start-Sleep -Seconds $WaitSeconds
-
-    Clear-Host
-}
-Set-Location $currentWorkingDirectory
-
-
-
-
-
-<#if ($ThemeFilePath) {
-    New-Item -Path $themeDirectory -ItemType Directory -Force | Select-Object FullName
-    Write-Host "Copy $ThemeFilePath to $themeDirectory/"
-    Copy-Item -Path $ThemeFilePath -Destination "$themeDirectory"
-}#>
-
-<# Write-Host "Removing theme directory $themeDirectory" -ForegroundColor green
- #>
